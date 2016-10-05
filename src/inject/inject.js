8c68d1bcdd7691539d751fb79ecd90f49195cb96
Adam Groves <adamgroves@Adams-MacBook-Pro.local>
2016-10-05 13:34:24 -0400

    Restructure and add inject.html

---
+++ src/inject/inject.js
@@ -0,0 +1,23 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Hello. This message was sent from src/inject/inject.js");
+
+			var xmlHttp = null;
+
+			xmlHttp = new XMLHttpRequest();
+			xmlHttp.open( "GET", chrome.extension.getURL ("src/inject/inject.html"), true );
+			xmlHttp.send( null );
+
+			var inject  = document.createElement("div");
+			inject.innerHTML = xmlHttp.responseText;
+			document.body.insertBefore (inject, document.body.firstChild);
+			// ----------------------------------------------------------
+
+		}
+		}, 10);
+});
