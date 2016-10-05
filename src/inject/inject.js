575f996b543677758245d0cc034822710a5321c8
Adam Groves <adamgroves@Adams-MacBook-Pro.local>
2016-10-05 17:26:39 -0400

    price advisor

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,8 +1,6 @@
 chrome.extension.sendMessage({}, function(response) {
 });
 
-chrome.extension.sendMessage({}, function(response) {
-});
 var year = 0;
 var make = "";
 var model = "";
@@ -22,7 +20,6 @@ function getMetaKeywords() {
 function checkForKeywords(data) {
    data = data.toLowerCase().split(" ");
    
-   console.log(JSON.stringify(data));
    if(data.length > 0){
 	   year = data[0];
    }
@@ -34,8 +31,7 @@ function checkForKeywords(data) {
    }
 }
 
-console.log("Hello. This message was sent from src/inject/inject.js");
-
+// Inject HTML into page
 var xmlHttp = null;
 
 xmlHttp = new XMLHttpRequest();
@@ -48,10 +44,33 @@ document.body.insertBefore (inject, document.body.firstChild);
 
 // Inject Ad
 var imgURL = chrome.extension.getURL("src/images/ad.png");
-console.log(imgURL)
 document.getElementById("kbb-ext-ad").src = imgURL;
 
+function getPriceAdvisor() {
+	var specs = document.getElementsByClassName("spec-key");
+	var vin = '';
+
+	for(var i = 0; i < specs.length; i++) {
+		if(specs[i].innerText === 'VIN:') {
+			vin = specs[i].nextElementSibling.innerText;
+		}
+	}
+	var pa_src = '';
+
+	// if (window.btData.stock === 'new') {
+	// 	pa_src = 'http://panc.syndication.kbb.com/priceadvisornewcar/fairpurchaseprice?APIKey=00000000-0000-0000-0000-000000000000&NewCarVIN='+vin+'&DisplayMSRP=true&DisplayAskingPrice=true&DisplayFPP=false&DisplayICO=true&DisplayTI=false&DisplaySRP=true';
+  //
+	// } else if (window.btData.stock === 'used' || window.btData.stock === 'cpo') {
+	// 	pa_src = 'http://pauc.syndication.kbb.com/priceadvisorusedcar/fairpurchaseprice?APIKey=00000000-0000-0000-0000-000000000000&NewCarVIN='+vin+'&DisplaySRP=true&DisplayAskingPrice=true';
+	// }
+
+	document.getElementById('kbb-ext-pa').setAttribute('src', pa_src);
+}
+
 setTimeout(function() {
+	getPriceAdvisor();
+
+	// Get keywords
 	var metakeywords = getMetaKeywords();
 
 	if(metakeywords.length > 0) {
@@ -61,7 +80,7 @@ setTimeout(function() {
 		checkForKeywords(document.title);
 	}
 	
-	console.log(year+ " " + make+ " " + model);
+	console.log("Year/Make/Model", year+ " " + make+ " " + model);
 	document.getElementById("year").value = year;
 	document.getElementById("make").value = make;
 	document.getElementById("model").value = model;
